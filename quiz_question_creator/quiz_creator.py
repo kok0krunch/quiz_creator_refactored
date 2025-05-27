295fc564859079b2a3b5c103548a1dd76ac617a8
kok0krunch <amaliakadoi@gmail.com>
2025-05-27 22:17:20 +0800

    Create QuestionCreator class inheriting from FileQuiz with constructor

--- quiz_question_creator/quiz_creator.py
+++ quiz_question_creator/quiz_creator.py
@@ -1,2 +1,7 @@
 import os
 from file_quiz import FileQuiz
+
+class QuestionCreator(FileQuiz):
+    def __init__(self, output_file="quiz_questions.txt"):
+        super().__init__(output_file)
+        self.output_file = output_file
