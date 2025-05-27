e99bb51b2b1a1e387f914eed235ed3fe42c5c952
kok0krunch <amaliakadoi@gmail.com>
2025-05-27 22:55:58 +0800

    Create QuizGame class inheriting from FileReader with constructor

--- quiz_game/quiz_game.py
+++ quiz_game/quiz_game.py
@@ -2,3 +2,7 @@ import random
 import sys
 from colorama import Fore, Style
 from file_reader import FileReader
+
+class QuizGame(FileReader):
+    def __init__(self, quiz_file="quiz_questions.txt"):
+        super().__init__(quiz_file)
